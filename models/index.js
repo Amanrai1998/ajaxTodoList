5442dcf82944634569dee4a2eeeb06e603112919
unknown <aman4pegasus@gmail.com>
2019-05-01 17:59:14 +0530

    update all dependencies and remove unneccesary dependencies

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.set("debug", true);
-mongoose.connect("mongodb://localhost/todos_api");
+mongoose.connect("mongodb://localhost/todos_api", {useNewUrlParser: true});
 mongoose.Promise = Promise;
 
 module.exports.Todo =  require("./todo");
